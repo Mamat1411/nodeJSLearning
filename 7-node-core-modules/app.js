1967471098e9e1fb00956cf79dfbe122c5000608
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-16 00:48:54 +0700

    Generating Files Asynchronously using fs.writeFile

--- 7-node-core-modules/app.js
+++ 7-node-core-modules/app.js
@@ -1,10 +1,19 @@
 //Core Modules
 //File System
+const { error } = require('console');
 const fs = require('fs');
 
 //Writing string to file (synchronous)
-try {
-    fs.writeFileSync('7-node-core-modules/testSync.txt', 'Hello World! (Synchronous)');
-} catch (error) {
-    console.log(error);
-}
+// try {
+//     fs.writeFileSync('7-node-core-modules/testSync.txt', 'Hello World! (Synchronous)');
+// } catch (error) {
+//     console.log(error);
+// }
+
+//Writing string to file (asynchronous)
+fs.writeFile('7-node-core-modules/testAsync.txt', 'Hello World! (Async)', (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log('The File has been saved');
+});
