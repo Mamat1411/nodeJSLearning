47f7c0fad89221cb19615fa37f54300fc1505b87
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-16 00:35:57 +0700

    Generating Files Synchronously using FS writeFileSync

---
+++ 7-node-core-modules/app.js
@@ -0,0 +1,6 @@
+//Core Modules
+//File System
+const fs = require('fs');
+
+//Writing string to file (synchronous)
+fs.writeFileSync('7-node-core-modules/testSync.txt', 'Hello World! (Synchronous)');
