6a76b67a9b867eba3b727794883445961855ac4a
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-16 02:17:13 +0700

    Simple Contact App Using FS and Readline Core Modules

--- 7-node-core-modules/app.js
+++ 7-node-core-modules/app.js
@@ -35,9 +35,21 @@ const { stdin: input, stdout: output } = require('process');
 
 const rl = readline.createInterface({input, output});
 
-rl.question('What do you think of Node.js? ', (answer) => {
-    rl.question('What is your name? ', (name) => {
-        console.log(`Thank you ${name} for your valuable feedback: ${answer}`);
+// rl.question('What do you think of Node.js? ', (answer) => {
+//     rl.question('What is your name? ', (name) => {
+//         console.log(`Thank you ${name} for your valuable feedback: ${answer}`);
+//         rl.close();
+//     });
+// });
+
+rl.question('What is your name? ', (name) => {
+    rl.question('Let me write your number for my contacts ', (number) => {
+        const contact = {name, number};
+        const file = fs.readFileSync('7-node-core-modules/contacts.json', 'utf-8');
+        const contacts = JSON.parse(file);
+        contacts.push(contact);
+        fs.writeFileSync('7-node-core-modules/contacts.json', JSON.stringify(contacts));
+        console.log(`Thank you ${name} for letting me have your number ${number}`);
         rl.close();
     });
 });
