b7d0701dd633dc18fa98262b20d7dc5a29d26f72
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-21 16:26:58 +0700

    Using Chalk Module

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,5 +1,11 @@
 const validator = require('validator');
+const chalk = require('chalk');
 
-console.log(validator.isEmail('muhammadmujahid14@gmail.com'));
-console.log(validator.isMobilePhone('081913036646', 'id-ID'));
-console.log(validator.isNumeric('081913036646'));
+// console.log(validator.isEmail('muhammadmujahid14@gmail.com'));
+// console.log(validator.isMobilePhone('081913036646', 'id-ID'));
+// console.log(validator.isNumeric('081913036646'));
+
+console.log(chalk.bold('Hello World'));
+const name = "Muhammad Mujahid";
+const message = chalk `Lorem ipsum dolor {bgRed.black sit amet} consectetur {bgGreen.black adipisicing} elit. Aliquid non amet debitis possimus aut commodi! My Name is {bgBlue.black ${name}}`;
+console.log(message);
