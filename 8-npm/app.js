404f3f87eae856b9c5713fe7a177ea708f7ba28e
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-21 16:25:26 +0700

    Using Validator Module

---
+++ 8-npm/app.js
@@ -0,0 +1,5 @@
+const validator = require('validator');
+
+console.log(validator.isEmail('muhammadmujahid14@gmail.com'));
+console.log(validator.isMobilePhone('081913036646', 'id-ID'));
+console.log(validator.isNumeric('081913036646'));
