2239dc77c571fdd924626088b77acd32199a4c31
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-23 03:44:18 +0700

    NPM Init on contact-app folder

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,8 +1,11 @@
-const {questions, saveContact} = require('./contacts');
-const main = async () => {
-    const name = await questions('What is your name? ');
-    const phoneNumber = await questions('Let me have your phone number so I can save it: ');
+// const {questions, saveContact} = require('./contacts');
+// const main = async () => {
+//     const name = await questions('What is your name? ');
+//     const phoneNumber = await questions('Let me have your phone number so I can save it: ');
 
-    saveContact(name, phoneNumber);
-};
-main();
+//     saveContact(name, phoneNumber);
+// };
+// main();
+
+//Simple Contact App with Different Approach(Data input using arguments in terminal)
+const command = process.argv[2];
