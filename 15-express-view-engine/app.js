4b57ba76a68e17b062cffbfb25e4feff01f43636
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-29 09:45:08 +0700

    EJS Simple Layouting

--- 15-express-view-engine/app.js
+++ 15-express-view-engine/app.js
@@ -6,15 +6,33 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-    response.render('index');
+    const employee = [
+        {
+            name : 'Muhammad Mujahid',
+            email: 'muhammadmujahid14@gmail.com'
+        },
+        {
+            name : 'Muhammad',
+            email: 'muhammad@gmail.com'
+        },
+        {
+            name : 'Mamat',
+            email: 'mamat@gmail.com'
+        }
+    ];
+    response.render('index', {
+        name : 'Muhammad Mujahid',
+        title: 'Express JS Home',
+        employee
+    });
 });
 
 app.get('/about', (request, response) => {
-    response.render('about');
+    response.render('about', { title: 'About Page' });
 });
 
 app.get('/contact', (request, response) => {
-    response.render('contact');
+    response.render('contact', { title: 'Contact Page' });
 });
 
 app.get('/product/:id', (request, response) => {
