6ec19d9db9457685723180383db1ed9f3663847c
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-29 08:55:26 +0700

    Installing EJS Templating Engine

--- 15-express-view-engine/app.js
+++ 15-express-view-engine/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//using EJS
+app.set('view engine', 'ejs');
+
 app.get('/', (request, response) => {
     response.sendFile('./index.html', { root: __dirname });
 });
