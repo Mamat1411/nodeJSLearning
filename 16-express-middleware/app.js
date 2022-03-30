c5c2c268351f98d3e70a209d0d8f4516dfa11f70
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-30 09:47:04 +0700

    Preparation for Chapter 16: Express Middleware

---
+++ 16-express-middleware/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 3000;
+
+//using EJS
+app.set('view engine', 'ejs');
+
+//using EJS Layouts Module
+app.use(expressLayouts);
+
+app.get('/', (request, response) => {
+    const employee = [
+        {
+            name : 'Muhammad Mujahid',
+            email: 'muhammadmujahid14@gmail.com'
+        },
+        {
+            name : 'Muhammad',
+            email: 'muhammad@gmail.com'
+        },
+        {
+            name : 'Mamat',
+            email: 'mamat@gmail.com'
+        }
+    ];
+    response.render('index', {
+        name : 'Muhammad Mujahid',
+        title: 'Express JS Home',
+        employee,
+        layout: 'layouts/main'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about', {
+        title: 'About Page',
+        layout: 'layouts/main'
+    });
+});
+
+app.get('/contact', (request, response) => {
+    response.render('contact', {
+        title: 'Contact Page',
+        layout: 'layouts/main'
+    });
+});
+
+app.get('/product/:id', (request, response) => {
+    response.send(`Product ID : ${request.params.id} <br> Category : ${request.query.category}`);
+});
+
+app.use('/', (request, response) => {
+    response.status(404);
+    response.send(`<h1>404</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Example App is listening to port ${port}`);
+});
