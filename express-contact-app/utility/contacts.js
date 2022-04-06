80c0cc62d91c5060d0025267c7aa33da4defff67
Mamat1411 <muhammadmujahid14@gmail.com>
2022-04-06 20:27:29 +0700

    Installing Validator and Chalk Module from NPM

---
+++ express-contact-app/utility/contacts.js
@@ -0,0 +1,14 @@
+const { error } = require('console');
+const fs = require('fs');
+const chalk = require('chalk');
+const validator = require('validator');
+
+if (!fs.existsSync('./contacts.json')) {
+    fs.writeFileSync('contacts.json', '[]', 'utf-8');
+}
+
+const loadContact = () => {
+    const file = fs.readFileSync('contacts.json', 'utf-8');
+    const contacts = JSON.parse(file);
+    return contacts;
+};
