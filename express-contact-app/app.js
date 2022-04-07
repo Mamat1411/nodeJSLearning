1cd1cbfbfdd2ab990fe2227070ec0001952e6f03
Mamat1411 <muhammadmujahid14@gmail.com>
2022-04-07 10:49:38 +0700

    Loading self-defined module => loadContact

--- express-contact-app/app.js
+++ express-contact-app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const { loadContact } = require('./utility/contacts.js');
 const app = express();
 const port = 3000;
 
