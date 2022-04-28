7978eab0ba5a591341d875997d92df3cda69594b
Mamat1411 <muhammadmujahid14@gmail.com>
2022-04-28 13:59:58 +0700

    Add Multiple Documents to db

--- 22-mongodb-and-nodejs/app.js
+++ 22-mongodb-and-nodejs/app.js
@@ -18,17 +18,42 @@ client.connect((err, client) => {
     const db = client.db(dbName);
 
     //Add 1 document into employee collection
-    db.collection('employee').insertOne(
-        {
-            name : "Muhammad",
-            email : "muhammad@gmail.com"
-        },
-        (err, result) => {
+    // db.collection('employee').insertOne(
+    //     {
+    //         name : "Muhammad",
+    //         email : "muhammad@gmail.com"
+    //     },
+    //     (err, result) => {
+    //         if (err) {
+    //             return console.log("Insert Data Failed");
+    //         }
+
+    //         console.log(result);
+    //     }
+    // );
+
+    //Add Multiple Documents into Employee Collection
+    db.collection('employee').insertMany(
+        [
+            {
+                name: "Maher",
+                email: "maher@gmail.com"
+            },
+            {
+                name: "Azza",
+                email: "azza@gmail.com"
+            },
+            {
+                name: "Mazen",
+                email: "mazen@gmail.com"
+            }
+        ],
+        (err, results) => {
             if (err) {
                 return console.log("Insert Data Failed");
             }
 
-            console.log(result);
+            console.log(results);
         }
     );
 });
