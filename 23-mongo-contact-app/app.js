01924ab39104a34cff191f7d6c3ab70a1066797c
Mamat1411 <muhammadmujahid14@gmail.com>
2022-05-18 14:08:38 +0700

    Copying Necessary Folders and Files from Previous Contact App

---
+++ 23-mongo-contact-app/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+const port = 3000;
+
+//Homepage
+app.get('/', (request, response) => {
+    const employee = [
+        {
+            name : 'Muhammad Mujahid',
+            email: 'muhammadmujahid14@gmail.com'
+        },
+        {
+            name : 'Muhammad',
+            email: 'muhammad@gmail.com'
+        },
+        {
+            name : 'Mamat',
+            email: 'mamat@gmail.com'
+        }
+    ];
+    response.render('index', {
+        name : 'Muhammad Mujahid',
+        title: 'Express JS Home',
+        employee,
+        layout: 'layouts/main'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Mongo Contact App is listening to http://localhost:${port}`);
+});
