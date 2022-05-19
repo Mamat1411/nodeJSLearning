931fbf67898e0a384c5045ca5f19bef676340275
Mamat1411 <muhammadmujahid14@gmail.com>
2022-05-19 10:23:26 +0700

    Deactivating Unused Codes

--- 23-mongo-contact-app/utility/db.js
+++ 23-mongo-contact-app/utility/db.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/dbEmployee');
 
-//Creating Schema
-const Contact = mongoose.model('Contact', {
-    name: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-    }
-});
+//Creating Schema (These lines of codes will be moved to ./models/contact.js once the database is successfully connected)
+// const Contact = mongoose.model('Contact', {
+//     name: {
+//         type: String,
+//         required: true,
+//     },
+//     phone: {
+//         type: String,
+//         required: true,
+//     },
+//     email: {
+//         type: String,
+//     }
+// });
 
-//Generating A Data (can be deactivated once the data is successfully generated)
-const contact1 = new Contact({
-    name: 'Muhammad Mujahid',
-    phone: '081913036646',
-    email: 'muhammadmujahid14@gmail.com'
-});
+// //Generating A Data (can be deactivated once the data is successfully generated)
+// const contact1 = new Contact({
+//     name: 'Muhammad Mujahid',
+//     phone: '081913036646',
+//     email: 'muhammadmujahid14@gmail.com'
+// });
 
-//Store The Generated Data into A Collection
-contact1.save()
-    .then((contact) => {
-        console.log(contact);
-    }).catch((err) => {
-        console.log(err);
-    });
+// //Store The Generated Data into A Collection
+// contact1.save()
+//     .then((contact) => {
+//         console.log(contact);
+//     }).catch((err) => {
+//         console.log(err);
+//     });
